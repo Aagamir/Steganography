c34a5c96eae18bb157a5579a452f242d1871da73
Mikołaj Marcinkowski <mikimarcinkowski@gmail.com>
2025-04-06 21:21:58 +0200

    Dodanie prostego interfejsu w cmd

--- main.py
+++ main.py
@@ -3,19 +3,17 @@ import matplotlib.pyplot as plt
 import stgsnek
 import stgfile
 
+mode = input("Co chcesz zrobić?\n1 - Zaszyfrować plik\n2 - Odszyfrować plik\n")
 
-def read_image(url):
-    image = plt.imread(url)
-    print(image.shape)
-    print(image.dtype)
-    plt.imshow(image[:, :, 0]%2)
-    plt.colorbar()
-    plt.show()
+if mode == "1":
+    input_image = str(input("Podaj nazwę pliku bez rozszerzenia w którym chcesz zakodować wiadomość:\n"))
+    print(input_image)
+    message = str(input("Podaj wiadomość którą chcesz zakodować w obrazie:\n"))
+    image = plt.imread(input_image+".jpg")
+    ImageEncoded = stgsnek.encode(image, message)
 
-image = plt.imread("rafał.jpg")
-ImageEncoded = stgsnek.encode(image, "Dzień dobry państu, z tej strony Remigiusz Maciaszek, co mam na czole, co mam na czole, twoją starą mam na czole")
-
-stgfile.save_encoded_image(ImageEncoded, "rafał")
-
-
-stgsnek.decode_utf8(stgfile.load_encoded_image("rafał"))
+    stgfile.save_encoded_image(ImageEncoded, input_image)
+elif mode == "2":
+    input_image = str(input("Podaj nazwę pliku bez rozszerzenia który chcesz odszyfrować:\n"))
+    print("Wiadomość ukryta w obrazie to:\n")
+    stgsnek.decode_utf8(stgfile.load_encoded_image(input_image))
