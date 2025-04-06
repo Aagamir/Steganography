8ff9df6a76f48ce06cb72a9cc23a9c76e0bc03e1
Mikołaj Marcinkowski <mikimarcinkowski@gmail.com>
2025-04-06 18:30:51 +0200

    Zaktualizowanie funkcji get_bin_char, get_bin_string i decode tak by obsugiway polskie znaki

--- main.py
+++ main.py
@@ -12,53 +12,76 @@ def read_image(url):
     plt.show()
 
 def get_bin_char(character):
-    x = [int(x) for x in bin(ord(character))[2:]]
-    x = [0]*(7-len(x)) + x #Dla polskich znaków będzie 9-len(x)
-    return x
+    utf8_bytes = character.encode('utf-8')
+    bits = []
+    for byte in utf8_bytes:
+        bits += [int(b) for b in format(byte, '08b')]
+    #x = [int(x) for x in bin(ord(character))[2:]]
+    #x = [0]*(7-len(x)) + x #Dla polskich znaków będzie 9-len(x)
+    #return x
+    return bits
 
 def get_bin_str(string):
     ret = []
     for character in string:
         ret += get_bin_char(character)
-    ret += [0]*7
+    ret += [0]*8
     return ret
 
-def encode(image, string):
+def encode(image, string, starting_point=0):
     x = get_bin_str(string)
     x = np.array(x, dtype=np.uint8)
 
     imageRound = image.copy()
     imageRound = image - image%2
     imageRound = imageRound.flatten()
-    imageRound[0:x.size] += x
+    imageRound[starting_point:x.size] += x
     
     if x.size > imageRound.size:
         raise ValueError("Obraz jest za mały, żeby zakodowac cały tekst")
 
     return np.reshape(imageRound, image.shape)
 
-def decode(image, read_every_n_bits = 7, start_point = 0):
-    places = np.array([2**(6-i) for i in range(7)])
+def decode(image, read_every_n_bits=8, start_point=0):
+    #places = np.array([2**(6-i) for i in range(7)])
     imageDecoded = image.flatten()
     n = read_every_n_bits
     i = start_point
     still_reading = True
     string = ""
+    byte_buffer = []
     while still_reading:
-        character = imageDecoded[i:i+n]%2
-        character = np.sum(places*character)
-        if character == 0:
+        byte = imageDecoded[i:i+n]%2
+        byte_val = int("".join(map(str, byte)), 2)
+        if byte_val == 0:
             still_reading = False
-        else:
-            string += chr(character)
-        i += 7
+        else: 
+            byte_buffer.append(byte_val)
+
+            try:
+                char = bytes(byte_buffer).decode("utf-8")
+                string += char
+                byte_buffer = []
+            except UnicodeDecodeError:
+                pass
+        i+=8
     print(string)
-    return(string)
+    return string
+
+        #character = imageDecoded[i:i+n]%2
+        #character = np.sum(places*character)
+        #if character == 0:
+        #    still_reading = False
+        #else:
+        #    string += chr(character)
+        #i += 7
+    #print(string)
+    #return(string)
 
 #ReadImage("Python\\Projekt\\test.jpg")
 #read_image("test.jpg")
 
 image = plt.imread("test.jpg")
-ImageEncoded = encode(image, "Ala ma kotą")
+ImageEncoded = encode(image, "Ala ma kotą,!/ i żółwia")
 
 decode(ImageEncoded)
