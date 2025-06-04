8532f7da9a3ff24a5139d34717034820e78b7e15
Mikołaj Marcinkowski <mikimarcinkowski@gmail.com>
2025-06-04 21:46:13 +0200

    Zmiana datecode

--- date_code.py
+++ date_code.py
@@ -6,10 +6,22 @@ class DateCipher:
         instance = super().__new__(cls)
         
         # Inicjalizacja atrybutów instancji
-        instance.date_format = "%Y-%m-%d %H:%M:%S"
-        instance.date_key = custom_date or datetime.now().strftime(instance.date_format)
+        instance.date_format = "%Y-%m-%d"
+        if custom_date:
+            # Walidacja formatu
+            try:
+                datetime.strptime(custom_date, instance.date_format)
+                instance.date_key = custom_date
+            except ValueError:
+                raise ValueError(f"Data musi być w formacie {instance.date_format} (np. 2025-06-04)")
+        else:
+            instance.date_key = datetime.now().strftime(instance.date_format
         
         return instance
+    
+    def get_date_key(self):
+        """Zwraca datę używaną jako klucz"""
+        return self.date_key
     
     def _generate_key(self):
         """Generuje klucz szyfrujący na podstawie daty"""
